6e0b49a9f5d607bc5bf8193385497500545f4b65
arpple <apple0239@gmail.com>
2022-11-02 00:19:50 +0900

    update deps, update config for intents

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,22 @@
-use Mix.Config
+import Config
+
+config :nostrum,
+	gateway_intents: [
+		:guilds,
+		:guild_bans,
+		:guild_emojis,
+		:guild_integrations,
+		:guild_webhooks,
+		:guild_invites,
+		:guild_voice_states,
+		:guild_messages,
+		:guild_message_reactions,
+		:guild_message_typing,
+		:direct_messages,
+		:direct_message_reactions,
+		:direct_message_typing,
+		:message_content,
+		:guild_scheduled_events
+	]
 
 import_config("secret.exs")
